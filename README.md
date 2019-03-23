ddaf3f71023d961fb9ca7f70d6100d79d894d04c
Risa Fujii <risaf1231@gmail.com>
2019-03-23 22:00:31 +0900

    Works in Lambda

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Image Processor
 
-This image processor can be used for resizing images to be used as thumbnails etc. The code can be used as-is, but is meant to be used in a AWS lambda instead of being run locally (I wanted to write the code locally and then upload to AWS).
+This image processor can be used for resizing images to be used as thumbnails etc.
+The code cannot be run as-is, because it's meant to be used in an AWS lambda instead of being run locally.
+
+Gem dependencies:
+- **AWS-SDK-S3** for downloading from and uploading to AWS S3 buckets
+
+- **Rmagick Version 2.16.0** for image manipulation. A newer version of Rmagick has been released, but I had to specify the version as this one because the newer version of Rmagick requires ImageMagick 6.8.9, which is not available to AWS Linux (the version that AWS Linux has is 6.7.8).
--- handler.rb
+++ handler.rb
@@ -1,6 +1,5 @@
 require 'aws-sdk-s3'
 require 'rmagick'
-require 'pry'
 
 include Magick
 
@@ -9,28 +8,30 @@ include Magick
 #   region: 'ap-northeast-1',
 #   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 #   })
+def lambda_handler(event:, context:)
+  # Client is necessary for downloading. Resource is for uploading.
+  s3_client = Aws::S3::Client.new
+  s3_resource = Aws::S3::Resource.new
 
-# Client is necessary for downloading. Resource is for uploading.
-s3_client = Aws::S3::Client.new
-s3_resource = Aws::S3::Resource.new
+  download_bucket_name = 'source-image-bois'
+  download_file_name = 'anarchy.jpg'
 
-download_bucket_name = 'source-image-bois'
-download_file_name = 'anarchy.jpg'
+  # This downloads an object to the specified path, or to memory (if no path specified).
+  # https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/S3/Client.html#get_object-instance_method
+  downloaded_file = s3_client.get_object(
+    response_target: '/tmp/resized.jpg',
+    bucket: download_bucket_name,
+    key: download_file_name
+    )
 
-# This downloads an object to the specified path, or to memory (if no path specified).
-# https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/S3/Client.html#get_object-instance_method
-downloaded_file = s3_client.get_object(
-  response_target: '/tmp/resized.jpg',
-  bucket: download_bucket_name,
-  key: download_file_name
-  )
+    # .first is because response is an array of images and we only want the conetnts of the first one.
+    original_image = Image.read('/tmp/resized.jpg').first
+    # https://rmagick.github.io/comtasks.html#thumb
+    processed_image = original_image.scale(125, 125)
+    processed_image.write('/tmp/resized.jpg')
 
-  # .first is because response is an array of images and we only want the conetnts of the first one.
-  original_image = Image.read('/tmp/resized.jpg').first
-  # https://rmagick.github.io/comtasks.html#thumb
-  processed_image = original_image.scale(125, 125)
-  processed_image.write('/tmp/resized.jpg')
+  # Fill this variable, and then call the upload method on the actual image in tmp.
+  upload_file_name = s3_resource.bucket('edited-image-bucket').object("resized_#{download_file_name}")
+  upload_file_name.upload_file('/tmp/resized.jpg')
 
-# Fill this variable, and then call the upload method on the actual image in tmp.
-upload_file_name = s3_resource.bucket('edited-image-bucket').object("resized_#{download_file_name}")
-upload_file_name.upload_file('/tmp/resized.jpg')
+end
